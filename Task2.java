2f0e934550d7e802329531028d4a3375cbd858cf
Ankit6121 <146847594+Ankit6121@users.noreply.github.com>
2023-10-14 12:09:31 +0530

    Update Calculator.java

--- Task2.java
+++ Task2.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class Task2 {
+public class StudentGradeCalculator {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("please enter the total number of the subject!: ");
